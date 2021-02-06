bd6d7633a74ecad43e96908039e2ea3f7e19b52f
ruveyharuzgar <ruveyharuzgar.108@gmail.com>
2021-02-06 16:11:54 +0300

    des
    des2-6şubat

---
+++ SQLQuery1.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE Cars(
+	Id int PRIMARY KEY IDENTITY(1,1),
+	BrandId int,
+	ColorId int,
+	ModelYear int,
+	DailyPrice int,
+	Description nvarchar(25),
+	FOREIGN KEY (ColorId) REFERENCES Colors(ColorId),
+	FOREIGN KEY (BrandId) REFERENCES Brands(BrandId)
+)
+CREATE TABLE Brands(
+	BrandId int PRIMARY KEY IDENTITY(1,1),
+	BrandName nvarchar(25),
+)
+CREATE TABLE Colors(
+	ColorId int PRIMARY KEY IDENTITY(1,1),
+	ColorName nvarchar(25),
+)
